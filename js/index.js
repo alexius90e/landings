921c9ed9a5797a2709933c5797140c0a08a69aba
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-02-10 22:40:11 +0300

    feat: implemented cases block

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.first-screen .swiper', {
+const firstScreenSwiper = new Swiper('.first-screen .swiper', {
   loop: true,
   pagination: {
     el: '.first-screen .swiper-pagination',
@@ -6,5 +6,29 @@ const swiper = new Swiper('.first-screen .swiper', {
   navigation: {
     nextEl: '.first-screen .swiper-button-next',
     prevEl: '.first-screen .swiper-button-prev',
+  },
+});
+
+const casesSwiper = new Swiper('.cases .swiper', {
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    568: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  pagination: {
+    el: '.cases .swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.cases__button-next',
+    prevEl: '.cases__button-prev',
   },
 });
