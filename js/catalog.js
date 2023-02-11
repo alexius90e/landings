4dd621c5c1d0077d62abd1412f0d6d50aa1de882
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-02-11 20:23:44 +0300

    feat: implemented custom select feature

---
+++ js/catalog.js
@@ -0,0 +1,9 @@
+import ItcCustomSelect from './itc-custom-select.js';
+
+const moduleSelect = new ItcCustomSelect('#moduleSelect');
+
+const gsmSelect = new ItcCustomSelect('#gsmSelect');
+
+const frequencySelect = new ItcCustomSelect('#frequencySelect');
+
+const supplySelect = new ItcCustomSelect('#supplySelect');
