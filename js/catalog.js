0c58dd3e3e006c69e349b37a3be6a7320ebbc734
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-02-11 21:17:49 +0300

    feat: implemented catalog sorting feature

--- js/catalog.js
+++ js/catalog.js
@@ -7,3 +7,9 @@ const gsmSelect = new ItcCustomSelect('#gsmSelect');
 const frequencySelect = new ItcCustomSelect('#frequencySelect');
 
 const supplySelect = new ItcCustomSelect('#supplySelect');
+
+const priceSelect = new ItcCustomSelect('#priceSelect');
+
+const ratingSelect = new ItcCustomSelect('#ratingSelect');
+
+const nameSelect = new ItcCustomSelect('#nameSelect');
