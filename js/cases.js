0d5a943c2a2ab11e87a080e3d109c6a1d232bea2
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-02-12 21:35:53 +0300

    feat: implemented cases card feature

--- js/cases.js
+++ js/cases.js
@@ -16,3 +16,45 @@ const casesPageSwiper = new Swiper('#casesPageSwiper', {
   slidesPerView: 1,
   spaceBetween: 20,
 });
+
+// modal
+
+const casesItems = document.querySelectorAll('.cases-page-slide');
+
+const casesModal = document.querySelector('#casesModal');
+const casesModalCloseButton = document.querySelector('#casesCloseBtn');
+
+casesModalCloseButton.addEventListener('click', () =>
+  casesModal.classList.remove('visible')
+);
+
+casesItems.forEach((item) => {
+  item.addEventListener('click', () => casesModal.classList.add('visible'));
+});
+
+casesModal.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal')) {
+    casesModal.classList.remove('visible');
+  }
+});
+
+// description
+
+const casesCardDescriptionToggler = document.querySelector(
+  '.cases-card-description__toggler'
+);
+
+const casesCardDescription = document.querySelector(
+  '.cases-card-description__text'
+);
+
+casesCardDescriptionToggler.addEventListener('click', (e) => {
+  const hiddenClassName = 'hidden';
+  casesCardDescription.classList.toggle(hiddenClassName);
+  const isHidden = casesCardDescription.classList.contains(hiddenClassName);
+  if (isHidden) {
+    casesCardDescriptionToggler.classList.add(hiddenClassName);
+  } else {
+    casesCardDescriptionToggler.classList.remove(hiddenClassName);
+  }
+});
