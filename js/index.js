4d9b05ab5607a8f08dd73e95bb90dd7897ab83ac
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-07-05 01:02:02 +0300

    fix: implemented advantages slider

--- js/index.js
+++ js/index.js
@@ -4,3 +4,21 @@ Fancybox.bind('[data-fancybox]', {
     showOnStart: false,
   },
 });
+
+const advantagesSwiper = new Swiper('.advantages .swiper', {
+  autoplay: {
+    delay: 2000,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    560: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+  navigation: {
+    nextEl: '.advantages .swiper-button-next',
+    prevEl: '.advantages .swiper-button-prev',
+  },
+});
