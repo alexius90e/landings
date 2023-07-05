b281d1552112d2779d929356a3fb8f740b0f2ec2
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-07-05 13:58:33 +0300

    feat: added correct gallery images

--- js/index.js
+++ js/index.js
@@ -23,11 +23,65 @@ const advantagesSwiper = new Swiper('.advantages .swiper', {
   },
 });
 
-const examplesBlock = document.querySelector('.examples');
+const examplesList = document.querySelector('.examples__list');
 
-const exampleIds = [...examplesBlock.querySelectorAll('.examples__list-item')].map(
-  (item) => item.id
-);
+const exampleItems = [
+  { id: 'exampleSwiperOne', countOfImages: 2 },
+  { id: 'exampleSwiperTwo', countOfImages: 4 },
+  { id: 'exampleSwiperThree', countOfImages: 13 },
+  { id: 'exampleSwiperFour', countOfImages: 7 },
+  { id: 'exampleSwiperFive', countOfImages: 16 },
+  { id: 'exampleSwiperSix', countOfImages: 14 },
+];
+
+const exampleIds = exampleItems.map((item) => item.id);
+
+const createSwiperSlide = (galleryIndex, slideIndex) => {
+  const swiperSlide = document.createElement('div');
+  swiperSlide.classList.add('swiper-slide');
+
+  const anchor = document.createElement('a');
+  anchor.href = `./assets/examples/example${galleryIndex + 1}-${slideIndex + 1}.jpg`;
+  anchor.dataset.fancybox = 'gallery' + galleryIndex;
+
+  const image = document.createElement('img');
+  image.setAttribute('src', anchor.href);
+  image.setAttribute('alt', 'example');
+
+  anchor.append(image);
+  swiperSlide.append(anchor);
+  return swiperSlide;
+};
+
+exampleItems.forEach((item, index) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('examples__list-item');
+  listItem.id = item.id;
+
+  const swiper = document.createElement('div');
+  swiper.classList.add('swiper');
+
+  const swiperWrapper = document.createElement('div');
+  swiperWrapper.classList.add('swiper-wrapper');
+
+  const swiperPagination = document.createElement('div');
+  swiperPagination.classList.add('swiper-pagination');
+
+  const swiperButtonPrev = document.createElement('div');
+  swiperButtonPrev.classList.add('swiper-button-prev');
+
+  const swiperButtonNext = document.createElement('div');
+  swiperButtonNext.classList.add('swiper-button-next');
+
+  for (let i = 0; i < item.countOfImages; i++) {
+    const slide = createSwiperSlide(index, i)
+    swiperWrapper.append(slide);
+  }
+
+  swiper.append(swiperWrapper, swiperPagination, swiperButtonPrev, swiperButtonNext);
+  listItem.append(swiper);
+  examplesList.append(listItem);
+});
 
 exampleIds.map(
   (id) =>
